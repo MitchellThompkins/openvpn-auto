d8121d76a38fa2a87e3816f810665cdc23e2b25d
Mitchell Thompkins <mitchell.thompkins@gmail.com>
2020-05-08 09:19:49 +0100

    Adding ability to pass username and passw

--- autoconnect.py
+++ autoconnect.py
@@ -9,18 +9,22 @@ import shutil
 
 class openVpn():
 
-    def __init__(self, vpnDirList, tcp, udp, vpnFile=None):
+    def __init__(self, vpnDirList, tcp, udp, user, passw, vpnFile=None):
         self.vpnDirList = vpnDirList
         self.tcp = tcp
         self.udp = udp
+        self.user = user
+        self.passw = passw
         self.vpnFile = vpnFile if vpnFile is not None else None
+
         self.fileList = {}
 
         if vpnDirList is None and vpnFile is None:
             sys.exit('Must specify either file or dir to collection of files')
 
         if tcp is False and udp is False and vpnFile is None:
-            sys.exit('At least one protocol must be specified')
+            sys.exit('At least one protocol must be specified if no vpn conf is\
+                    specified')
 
         self.getVpnFiles()
 
@@ -40,16 +44,25 @@ class openVpn():
         if self.vpnFile is None:
             self.getRandomVpnFile()
 
+        # Create a path to the vpn configuration
         path = os.path.join(self.vpnDirList, self.vpnFile)
 
-        editFile = 'auth-user-pass /etc/openvpn/openvpn_auth.auth'
 
         try:
             tmpVpnFile = tempfile.NamedTemporaryFile(delete=True)
+            tmpVpnConf = tempfile.NamedTemporaryFile(delete=True)
+
+            with open(tmpVpnConf.name, 'w') as f:
+                f.write(self.user)
+                f.write('\n')
+                f.write(self.passw)
+
+            #authStr = 'auth-user-pass /etc/openvpn/openvpn_auth.auth'
+            authStr = 'auth-user-pass' + tmpVpnConf.name
 
             with open(path, 'r') as f:
                 c = f.read()
-                cNew = re.sub(r'\bauth-user-pass\b', editFile, c, flags = re.M)
+                cNew = re.sub(r'\bauth-user-pass\b', authStr, c, flags = re.M)
 
                 with open(tmpVpnFile.name, 'w') as v:
                     v.write(cNew)
@@ -59,23 +72,35 @@ class openVpn():
         except Exception as e:
             print(e)
 
+    def createAuthFile(self):
+        pass
+
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Get autoconnect openvpn options')
 
     # Argument List
-    parser.add_argument('--dir', '-d',  type=str,\
-            help='Directory containing collection of openvpn connections')
     parser.add_argument('--file', '-f',  type=argparse.FileType('r'),\
             help='Specify openvpn file to use. Takes precedence over --dir')
-    parser.add_argument('--tcp', '-t', action='store_true',\
-            help='Allow tcp connections')
-    parser.add_argument('--udp', '-u', action='store_true',\
-            help='Allow udp connections')
+
+    parser.add_argument('--dir', '-d',  type=str,\
+            help='Directory containing collection of openvpn connections')
+
+    parser.add_argument('--tcp', action='store_true',\
+            help='Allow random selection of tcp connections')
+
+    parser.add_argument('--udp', action='store_true',\
+            help='Allow random selection of udp connections')
+    
+    parser.add_argument('--user', '-u', type=str,\
+            help='Pass username for vpn authentication')
+    
+    parser.add_argument('--passw', '-p', type=str,\
+            help='Pass username for vpn authentication')
 
     args = parser.parse_args()
 
-    vpn = openVpn( args.dir, args.tcp, args.udp, args.file )
+    vpn = openVpn( args.dir, args.tcp, args.udp, args.user, args.passw, args.file  )
     vpn.connect()
 
